e24bbbd6784e642c8669641a2cb0c61b32a34547
emiridbest <udogwuemiri@gmail.com>
2023-09-16 15:24:53 +0100

    5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,182 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+import { contractABI, AltheaAds } from "./utils/const.js";
+import AddNFTForm from "./components/AddNFTForm";
+import Navbar from "./components/Navbar.js";
+import Footer from "./components/Footer.js";
+import { altheaTokenABI, altheaTokenAddress } from "./utils/altToken";
+import ReactSpeedometer from "react-d3-speedometer";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [account, setAccount] = useState(null);
+  const [altheaAdsContract, setAltheaAdsContract] = useState(null);
+  const [altTokenContract, setAltTokenContract] = useState(null);
+  const [adScript, setAdScript] = useState("");
+  const [clicks, setClicks] = useState(0);
+  const [impressions, setImpressions] = useState(0);
+  const [altBalance, setAltBalance] = useState(0);
+  const maxValue = 1000;
+  const maxValue1 = 100;
+  useEffect(() => {
+    initializeWeb3();
+  }, []);
+
+  const initializeWeb3 = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.enable();
+        const web3 = new Web3(window.ethereum);
+
+        const accounts = await web3.eth.getAccounts();
+        const instance = new web3.eth.Contract(contractABI, AltheaAds);
+        const altInstance = new web3.eth.Contract(
+          altheaTokenABI,
+          altheaTokenAddress
+        );
+
+        setAccount(accounts[0]);
+        setAltheaAdsContract(instance);
+        setAltTokenContract(altInstance);
+      } catch (error) {
+        console.error("Error initializing web3:", error);
+      }
+    } else {
+      console.log("MetaMask extension not found.");
+    }
+  };
+
+  const handleButtonClick = async () => {
+    if (altheaAdsContract) {
+      const adId = await altheaAdsContract.methods.getRandomAd().call();
+      const ad = {
+        id: adId[0],
+        name: adId[1],
+        description: adId[2],
+        pinataCID: adId[3],
+      };
+      const url = `https://ipfs.io/ipfs/${ad.pinataCID}`;
+
+      const script = `<script>
+        window.addEventListener('DOMContentLoaded', (event) => {
+          const adContainer = document.getElementById('ad-container');
+          if(adContainer){
+            adContainer.innerHTML = '<iframe src="${url}" width="600" height="500"></iframe>';
+          }
+        });
+      </script>`;
+
+      console.log(script);
+      setAdScript(script);
+    }
+  };
+
+  const handleAddNFT = async ({ name, description, pinataCID }) => {
+    if (altheaAdsContract) {
+      await altheaAdsContract.methods
+        .addNft(name, description, pinataCID)
+        .send({ from: account });
+      alert("NFT added");
+    }
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const fetchAdStats = async () => {
+    const adClicks = await altheaAdsContract.methods
+      .referrerClicks(account)
+      .call();
+    const adImpressions = await altheaAdsContract.methods
+      .referrerImpressions(account)
+      .call();
+   // const balance = await altTokenContract.methods.balanceOf(account).call();
+    setClicks(Number(adClicks));
+    setImpressions(Number(adImpressions));
+    //setAltBalance(Number(balance));
+  };
+  useEffect(() => {
+    if (altheaAdsContract && altTokenContract && account) {
+      fetchAdStats();
+    }
+  }, [altheaAdsContract, altTokenContract, account, fetchAdStats]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      {account ? (
+        <div>
+          <p className="account">Connected account: {account}</p>
+          <div className="welcome">
+            Bringing Althea layer 1 Blockchain to you Using <br />
+            NFT-Powered Ads
+          </div>
+          <div className="ad-stats-container">
+            <div className="grid-column">
+              <div className="grid-box">
+                1Million+ <br /> Users
+              </div>
+              <div className="grid-box">
+                $10Mil <br /> Paid
+              </div>
+              <div className="grid-box">
+                77+ <br /> Countries
+              </div>
+              <div className="grid-box">
+                Over 5 <br /> Continents
+              </div>
+            </div>
+            <div className="stats-column ">
+              <div className="impress">
+                <ReactSpeedometer
+                  maxValue={maxValue1}
+                  value={clicks}
+                  currentValueText={`Clicks: ${clicks}`}
+                  width={300}
+                  height={175}
+                  needleHeightRatio={0.7}
+                  segments={5}
+                  startColor="#013655"
+                  endColor="#0b2230"
+                  textColor="#ffffff"
+                />
+                <ReactSpeedometer
+                  maxValue={maxValue}
+                  value={impressions}
+                  currentValueText={`Impressions: ${impressions}`}
+                  width={300}
+                  height={175}
+                  needleHeightRatio={0.7}
+                  segments={5}
+                  startColor="#013655"
+                  endColor="#0b2230"
+                  textColor="#ffffff"
+                />
+              </div>{" "}
+              <div className="comp">
+                <p className="token">ALT Token Balance: {altBalance}</p>
+              </div>{" "}
+            </div>
+          </div>
+          <div className="form">
+            <AddNFTForm onAddNFT={handleAddNFT} />
+          </div>
+          <div className="get">
+            <button onClick={handleButtonClick} className="get">
+              Get Random Ad
+            </button>
+            <textarea value={adScript} readOnly />
+          </div>
+          <div>
+            {" "}
+            <Footer />
+          </div>
+        </div>
+      ) : (
+        <p className="connect">Please connect your Althea Wallet Address.</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/utils/const.js
@@ -0,0 +1,152 @@
+export const IERC20 ="0xA11B61e3886b2749c566A34262E63915CFD03800"
+export  const IERC721 ="0xf99cb3Bc6a652DAD49AD0cf3389910eeA658e567"
+export const AltheaAds ="0x2241480c9196367E4335EE418D06d6990fC469C7"
+
+export const contractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "contract IERC20",
+				"name": "altheaToken",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "description",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "pinataCID",
+				"type": "string"
+			}
+		],
+		"name": "addNft",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getRandomAd",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "uint256",
+						"name": "id",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "name",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "description",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "pinataCID",
+						"type": "string"
+					}
+				],
+				"internalType": "struct AltheaAds.Advertisement",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "referrerClicks",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "referrerImpressions",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "referrer",
+				"type": "address"
+			}
+		],
+		"name": "registerClick",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "referrer",
+				"type": "address"
+			}
+		],
+		"name": "registerImpression",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
